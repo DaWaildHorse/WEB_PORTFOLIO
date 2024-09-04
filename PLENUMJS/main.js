32a8de8d11a0dd401d8bcf5ecd493cc0fc95c21b
DaWaildHorse <andeelou69@outlook.com>
2024-09-04 11:49:21 -0600

    wip : doing some exercises to not forget JS

--- PLENUMJS/main.js
+++ PLENUMJS/main.js
@@ -1,24 +1,55 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const Ej1 = (str = "")  => (!str)? console.warn("Mete una cadena") :
+  console.info("La cadena tiene una long de : " + str.length)
 
-setupCounter(document.querySelector('#counter'))
+
+const Ej2 = (str = "" , n) => console.info(str.slice(0,n))
+
+function Ej3(str = "", splicer){
+  var spliced_str = str.split(splicer)
+  console.info(spliced_str)
+}
+
+function Ej4(str = "", n){
+  for (let i = 0; i < n; i++) {
+    console.log(str)
+  }
+}
+
+function Ej5(str = ""){
+  str = str.split("").reverse().join("");
+  console.info(str);
+}
+
+
+function Ej6(str = "" , text = ""){
+  let i = 0 , cont = 0
+  while(i !== -1 ){
+    i = str.indexOf(text , i)
+    if(i !== -1){
+      i++
+      cont++
+    }
+  }
+  console.log("Se repitio " + cont + " veces ")
+}
+
+function Ej7(str = ""){
+  str = str.toLocaleLowerCase()
+  inverted_str = str.split("").reverse().join("");
+  (inverted_str === str)? console.log("Es palindromo") : console.log("No es palindromo")
+}
+
+let str = 'hannaH'
+
+Ej1(str)
+
+Ej2(str, 11)
+
+Ej3(str , "e")
+
+Ej5(str)
+
+Ej6(str , "Hello")
+
+Ej7(str)
