b98d5611529fd7540f2f138716ab3c879c77c3d6
Andre <andeelou69@outlook.com>
2024-09-08 22:51:57 -0600

    DOM to remember

---
+++ PLENUMJS/DOM.js
@@ -0,0 +1,13 @@
+
+/*
+const utterance = new SpeechSynthesisUtterance("Hola que tal")
+speechSynthesis.speak(utterance)
+*/
+
+setTimeout(() =>{
+    console.log(document.getSelection().toString())
+}, 3000)
+
+//escribir 
+document.write("<h3> Hola mundo de js <h3>")
+
